80686bac2b0cee6e86a69d8c1976e142aa85fde5
Ricardo Sánchez-Sáez <rsanchez.saez@gmail.com>
2013-09-01 00:33:58 +0100

    Added part B of tutorial 2.

---
+++ gltut/gltut 02b/Scene.cpp
@@ -0,0 +1,73 @@
+//
+//  Scene.cpp
+//  gltut-glfw
+//
+//  Created by Ricardo Sánchez-Sáez on 16/08/2013.
+//  Copyright (c) 2013 rsanchezsaez. All rights reserved.
+//
+
+#include "Scene.h"
+
+#include <string>
+#include <vector>
+
+#include "debug.h"
+#include "glhelpers.h"
+
+const float vertexData[] = {
+    0.0f,    0.5f, 0.0f, 1.0f,
+    0.5f, -0.366f, 0.0f, 1.0f,
+	-0.5f, -0.366f, 0.0f, 1.0f,
+    1.0f,    0.0f, 0.0f, 1.0f,
+    0.0f,    1.0f, 0.0f, 1.0f,
+    0.0f,    0.0f, 1.0f, 1.0f,
+};
+
+Scene::Scene()
+{
+}
+
+void Scene::init()
+{
+    _shaderProgram = createShaderProgramWithFilenames("VertexColors.vert", "VertexColors.frag");
+    glUseProgram(_shaderProgram);
+    printOpenGLError();
+    
+    // Initialize Vertex Buffer
+    glGenBuffers(1, &_positionBufferObject);
+    
+	glBindBuffer(GL_ARRAY_BUFFER, _positionBufferObject);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+    printOpenGLError();
+    
+    // Vertex array object
+    glGenVertexArrays(1, &_vertexArrayObject);
+	glBindVertexArray(_vertexArrayObject);
+    printOpenGLError();
+}
+
+Scene::~Scene()
+{
+    glDeleteProgram(_shaderProgram);
+    glDeleteBuffers(1, &_positionBufferObject);
+    printOpenGLError();
+}
+
+void Scene::draw()
+{
+    glClearColor(0.2f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+	glBindBuffer(GL_ARRAY_BUFFER, _positionBufferObject);    
+    glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void*)48);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+    printOpenGLError();
+    
+	glDisableVertexAttribArray(0);
+    printOpenGLError();
+}
