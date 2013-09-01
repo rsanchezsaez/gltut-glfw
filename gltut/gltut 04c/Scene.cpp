eb652f1678363560c10c659f53f1c83bb0d4e209
Ricardo Sánchez-Sáez <rsanchez.saez@gmail.com>
2013-09-01 18:32:17 +0100

    Added third tutorial for Chapter 4.

---
+++ gltut/gltut 04c/Scene.cpp
@@ -0,0 +1,206 @@
+//
+//  Scene.cpp
+//  gltut-glfw
+//
+// Copyright (C) 2010-2012 by Jason L. McKesson
+// Xcode and glfw adaptation: Ricardo Sánchez-Sáez.
+//
+// This file is licensed under the MIT License.
+//
+
+#include "Scene.h"
+
+#include <string>
+#include <vector>
+
+#include "debug.h"
+#include "glhelpers.h"
+#include "GLFW/glfw3.h"
+
+#include <math.h>
+
+#define ARRAY_COUNT( array ) (sizeof( array ) / (sizeof( array[0] ) * (sizeof( array ) != sizeof(void*) || sizeof( array[0] ) <= sizeof(void*))))
+
+const float vertexData[] = {
+    0.25f,  0.25f, -1.25f, 1.0f,
+    0.25f, -0.25f, -1.25f, 1.0f,
+	-0.25f,  0.25f, -1.25f, 1.0f,
+    
+    0.25f, -0.25f, -1.25f, 1.0f,
+	-0.25f, -0.25f, -1.25f, 1.0f,
+	-0.25f,  0.25f, -1.25f, 1.0f,
+    
+    0.25f,  0.25f, -2.75f, 1.0f,
+	-0.25f,  0.25f, -2.75f, 1.0f,
+    0.25f, -0.25f, -2.75f, 1.0f,
+    
+    0.25f, -0.25f, -2.75f, 1.0f,
+	-0.25f,  0.25f, -2.75f, 1.0f,
+	-0.25f, -0.25f, -2.75f, 1.0f,
+    
+	-0.25f,  0.25f, -1.25f, 1.0f,
+	-0.25f, -0.25f, -1.25f, 1.0f,
+	-0.25f, -0.25f, -2.75f, 1.0f,
+    
+	-0.25f,  0.25f, -1.25f, 1.0f,
+	-0.25f, -0.25f, -2.75f, 1.0f,
+	-0.25f,  0.25f, -2.75f, 1.0f,
+    
+    0.25f,  0.25f, -1.25f, 1.0f,
+    0.25f, -0.25f, -2.75f, 1.0f,
+    0.25f, -0.25f, -1.25f, 1.0f,
+    
+    0.25f,  0.25f, -1.25f, 1.0f,
+    0.25f,  0.25f, -2.75f, 1.0f,
+    0.25f, -0.25f, -2.75f, 1.0f,
+    
+    0.25f,  0.25f, -2.75f, 1.0f,
+    0.25f,  0.25f, -1.25f, 1.0f,
+	-0.25f,  0.25f, -1.25f, 1.0f,
+    
+    0.25f,  0.25f, -2.75f, 1.0f,
+	-0.25f,  0.25f, -1.25f, 1.0f,
+	-0.25f,  0.25f, -2.75f, 1.0f,
+    
+    0.25f, -0.25f, -2.75f, 1.0f,
+	-0.25f, -0.25f, -1.25f, 1.0f,
+    0.25f, -0.25f, -1.25f, 1.0f,
+    
+    0.25f, -0.25f, -2.75f, 1.0f,
+	-0.25f, -0.25f, -2.75f, 1.0f,
+	-0.25f, -0.25f, -1.25f, 1.0f,
+    
+    
+    
+    
+	0.0f, 0.0f, 1.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
+    
+	0.0f, 0.0f, 1.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
+    
+	0.8f, 0.8f, 0.8f, 1.0f,
+	0.8f, 0.8f, 0.8f, 1.0f,
+	0.8f, 0.8f, 0.8f, 1.0f,
+    
+	0.8f, 0.8f, 0.8f, 1.0f,
+	0.8f, 0.8f, 0.8f, 1.0f,
+	0.8f, 0.8f, 0.8f, 1.0f,
+    
+	0.0f, 1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 0.0f, 1.0f,
+    
+	0.0f, 1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 0.0f, 1.0f,
+    
+	0.5f, 0.5f, 0.0f, 1.0f,
+	0.5f, 0.5f, 0.0f, 1.0f,
+	0.5f, 0.5f, 0.0f, 1.0f,
+    
+	0.5f, 0.5f, 0.0f, 1.0f,
+	0.5f, 0.5f, 0.0f, 1.0f,
+	0.5f, 0.5f, 0.0f, 1.0f,
+    
+	1.0f, 0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 0.0f, 1.0f,
+    
+	1.0f, 0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 0.0f, 1.0f,
+    
+	0.0f, 1.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f, 1.0f,
+    
+	0.0f, 1.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f, 1.0f,
+    
+};
+
+GLuint offsetUniform;
+
+GLuint perspectiveMatrixUniform;
+
+Scene::Scene()
+{
+}
+
+void Scene::init()
+{
+    _shaderProgram = createShaderProgramWithFilenames("MatrixPerspective.vert", "StandardColors.frag");
+    glUseProgram(_shaderProgram);
+    printOpenGLError();
+    
+    // Initialize Vertex Buffer
+    glGenBuffers(1, &_vertexBufferObject);
+    
+	glBindBuffer(GL_ARRAY_BUFFER, _vertexBufferObject);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+    printOpenGLError();
+    
+    // Vertex array object
+    glGenVertexArrays(1, &_vertexArrayObject);
+	glBindVertexArray(_vertexArrayObject);
+    printOpenGLError();
+
+    // Enable cull facing
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CW);
+
+    // Uniforms
+	offsetUniform = glGetUniformLocation(_shaderProgram, "offset");
+    
+	perspectiveMatrixUniform = glGetUniformLocation(_shaderProgram, "perspectiveMatrix");
+    
+	float fFrustumScale = 1.0f; float fzNear = 0.5f; float fzFar = 3.0f;
+    
+	float theMatrix[16];
+	memset(theMatrix, 0, sizeof(float) * 16);
+    
+	theMatrix[0] = fFrustumScale;
+	theMatrix[5] = fFrustumScale;
+	theMatrix[10] = (fzFar + fzNear) / (fzNear - fzFar);
+	theMatrix[14] = (2 * fzFar * fzNear) / (fzNear - fzFar);
+	theMatrix[11] = -1.0f;
+    
+	glUniformMatrix4fv(perspectiveMatrixUniform, 1, GL_FALSE, theMatrix);
+    
+}
+
+Scene::~Scene()
+{
+    glDeleteProgram(_shaderProgram);
+    glDeleteBuffers(1, &_vertexBufferObject);
+    printOpenGLError();
+}
+
+void Scene::draw()
+{
+    glClearColor(0.2f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+	glUniform2f(offsetUniform, 0.5f, 0.5f);
+
+	size_t colorData = sizeof(vertexData) / 2;
+	glBindBuffer(GL_ARRAY_BUFFER, _vertexBufferObject);
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void*)colorData);
+    printOpenGLError();
+
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+    printOpenGLError();
+
+	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
+    printOpenGLError();
+}
