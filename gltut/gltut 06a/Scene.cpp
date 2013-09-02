5d62e04d8fc7476f6d5b34a68abbc82a2976d18d
Ricardo Sánchez-Sáez <rsanchez.saez@gmail.com>
2013-09-02 22:57:25 +0100

    gltut 06: Added proper frustum scale.

--- gltut/gltut 06a/Scene.cpp
+++ gltut/gltut 06a/Scene.cpp
@@ -70,8 +70,14 @@ glm::mat4 cameraToClipMatrix(0.0f);
 GLuint modelToCameraMatrixUniform;
 GLuint cameraToClipMatrixUniform;
 
-float perspectiveMatrix[16];
-const float frustumScale = 1.0f;
+float CalcFrustumScale(float fFovDeg)
+{
+	const float degToRad = 3.14159f * 2.0f / 360.0f;
+	float fFovRad = fFovDeg * degToRad;
+	return 1.0f / tan(fFovRad / 2.0f);
+}
+
+const float frustumScale = CalcFrustumScale(45.0f);
 
 GLuint indexBufferObject;
 
